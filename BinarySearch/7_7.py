2d2ccd6eed7e2e8f500e6fdf75f8c054579d86f5
haeun1 <haeun1120@konkuk.ac.kr>
2023-10-10 04:37:38 +0900

    이진 탐색1

---
+++ BinarySearch/7_7.py
@@ -0,0 +1,13 @@
+#부품 찾기(set 자료형)
+
+n= int(input())
+array = set(map(int,input().split()))
+
+m = int(input())
+x = list(map(int,input().split()))
+
+for i in x:
+    if i in array:
+        print('yes')
+    else:
+        print('no')
